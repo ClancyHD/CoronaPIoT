0b46c1fade19f1b54eb1e553dc1e91a13a583022
Lukas Bernard <Lukas.Bernard@outlook.com>
2020-10-08 14:05:46 +0200

    Create solution and core project

---
+++ CoronaPIoTCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoronaPIoTCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
